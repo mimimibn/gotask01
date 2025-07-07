b254380a6eeaabf1ef00ac8dd389188e27921ccd
muchunyue <123@qq.com>
2025-07-07 09:36:05 +0800

    增加方法

--- task01.go
+++ task01.go
@@ -8,14 +8,14 @@ import (
 )
 
 func main() {
-	//func1()
-	//func2()
-	//func3()
-	//func4()
-	//func5()
-	//func6()
+	func1()
+	func2()
+	func3()
+	func4()
+	func5()
+	func6()
 	func7()
-	//func8()
+	func8()
 }
 
 /*
